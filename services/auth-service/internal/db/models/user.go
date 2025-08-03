8d82209e8d86d9a8d9963d4d4973034407ad3899
khlyazzat <lyly.kh@yandex.kz>
2025-08-03 19:17:53 +0500

    change models

--- services/auth-service/internal/db/models/user.go
+++ services/auth-service/internal/db/models/user.go
@@ -9,10 +9,8 @@ import (
 type User struct {
 	bun.BaseModel `bun:"table:users,alias:u"`
 	ID            int64 `bun:",pk,autoincrement"`
-	Name          string
 	Password      string
 	Email         string
-	Age           int
 	CreatedAt     time.Time
 	UpdatedAt     bun.NullTime
 	DeletedAt     bun.NullTime `bun:",soft_delete,nullzero"`
