8d06de2025793d44058d68c8f71ae7367683b13a
khlyazzat <lyly.kh@yandex.kz>
2025-08-03 19:19:05 +0500

    change table

--- services/auth-service/migrations/000001_create_users_table.up.sql
+++ services/auth-service/migrations/000001_create_users_table.up.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   id          BIGSERIAL PRIMARY KEY,
-  name        TEXT NOT NULL,
-  password    TEXT,
   email       TEXT UNIQUE NOT NULL,
-  age         INT,
+  password    TEXT NOT NULL,
 	created_at  TIMESTAMP WITHOUT TIME ZONE,
 	updated_at  TIMESTAMP WITHOUT TIME ZONE,
 	deleted_at  TIMESTAMP WITHOUT TIME ZONE
